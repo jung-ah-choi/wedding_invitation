2b94ad91d395f8daa5548846f1a957a2936bdbd7
JungAh Choi (최정아) <wjddk718@gmail.com>
2024-04-09 01:51:51 +0900

    리렌더링 최적화

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function App() {
   const [wedding, setWedding] = useState<Wedding | null>(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
+  const [count, setCount] = useState(0)
 
   // 1. wedding data
   useEffect(() => {
@@ -71,6 +72,14 @@ function App() {
 
   return (
     <div className={cx('container')}>
+      <button
+        style={{ position: 'fixed', top: 0 }}
+        onClick={() => {
+          setCount((prev) => prev + 1)
+        }}
+      >
+        + {count}
+      </button>
       <Heading date={date} />
       <Video />
       <Intro
