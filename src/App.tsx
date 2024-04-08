2a1e4369c40b34e53014053e1f45b6665c01f0a2
JungAh Choi (최정아) <wjddk718@gmail.com>
2024-04-08 17:36:28 +0900

    참석 여부 모달 추가

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import Calendar from './components/sections/Calendar'
 import Map from './components/sections/Map'
 import Contact from './components/sections/Contact'
 import Share from './components/sections/Share'
+import AttendCountModal from './components/AttendCountModal'
 
 const cx = classNames.bind(styles)
 
@@ -85,6 +86,7 @@ function App() {
       <Map location={location} />
       <Contact groom={groom} bride={bride} />
       <Share groomName={groom.name} brideName={bride.name} date={date} />
+      <AttendCountModal wedding={wedding} />
     </div>
   )
 }
---
+++ src/contexts/ModalContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, ComponentProps, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+import Modal from '@/components/shared/Modal'
+
+type ModalProps = ComponentProps<typeof Modal>
+type ModalOptions = Omit<ModalProps, 'open'>
+
+interface ModalContextValue {
+  open: (options: ModalOptions) => void
+  close: () => void
+}
+
+const Context = createContext<ModalContextValue | undefined>(undefined)
+
+const defaultValues: ModalProps = {
+  open: false,
+  body: null,
+  onRightButtonClick: () => {},
+  onLeftButtonClick: () => {},
+}
+
+export function ModalContext({ children }: { children: React.ReactNode }) {
+  const [modalState, setModalState] = useState<ModalProps>(defaultValues)
+
+  const $portal_root = document.getElementById('root-portal')
+
+  const open = (options: ModalOptions) => {
+    setModalState({ ...options, open: true })
+  }
+
+  const close = () => {
+    setModalState(defaultValues)
+  }
+
+  const values = {
+    open,
+    close,
+  }
+
+  return (
+    <Context.Provider value={values}>
+      {children}
+      {$portal_root != null
+        ? createPortal(<Modal {...modalState} />, $portal_root)
+        : null}
+    </Context.Provider>
+  )
+}
+
+// Modal Context로 감싸지 않고 사용하는 경우 방지
+export function useModalContext() {
+  const values = useContext(Context)
+
+  if (values == null) {
+    throw new Error('ModalContext 안에서 사용해주세요')
+  }
+
+  return values
+}
